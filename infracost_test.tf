9d8c27349066c7f5d1c5585cf151ad8303421194
benjaminfrju <109394113+benjaminfrju@users.noreply.github.com>
2025-01-30 02:14:09 -0600

    Update infracost_test.tf

--- infracost_test.tf
+++ infracost_test.tf
@@ -1,45 +1,69 @@
-provider "google" {
-  region = "us-central1"
-  project = "test"
+## Connection variables
+project_id                  = "fine-web-382122"
+google_credential_file_path = "./gcp_credentials.json"
+
+## VPC and deployment environment variables
+vpc_name        = "msgcp"
+kasm_vpc_subnet = "10.0.0.0/16"
+
+## Ensure the desired Database region is the first value in the list
+kasm_deployment_regions = ["us-east1"] # Use only one region for Multi-Server (single-region)
+#kasm_deployment_regions = ["us-west2", "asia-southeast1"]   # Use multiple regions for Multi-Region deployment
+
+## DNS Zone settings
+create_public_dns_zone    = true
+public_dns_friendly_name  = "kasm-public-dns-zone"
+private_dns_friendly_name = "kasm-private-dns-zone"
+
+## Additional Kasm services or GCP features to deploy
+create_kasm_autoscale_service_account = true
+service_account_name                  = "kmiee-dev-ms-cloudvdi"
+show_passwords                        = true
+
+## Kasm variables
+kasm_domain_name  = "gooogle.blackhost.mx"
+kasm_project_name = ""
+deployment_type   = "Multi-Server" # Valid values Multi-Region or Multi-Server
+kasm_version      = "1.16.1"
+kasm_download_url = "https://kasm-static-content.s3.amazonaws.com/kasm_release_1.16.1.98d6fa.tar.gz"
+
+## Kasm VM instance configurations
+# Webapp
+webapp_vm_instance_config = {
+  machine_type = "e2-standard-2"
+  disk_size_gb = "50"
+  disk_type    = "pd-standard"
 }
 
-resource "google_compute_instance" "my_instance" {
-  zone = "us-central1-a"
-  name = "test"
-
-  machine_type = "n1-standard-16" # <<<<<<<<<< Try changing this to n1-standard-32 to compare the costs
-  network_interface {
-    network = "default"
-    access_config {}
-  }
-
-  boot_disk {
-    initialize_params {
-      image = "debian-cloud/debian-9"
-    }
-  }
-
-  scheduling {
-    preemptible = true
-  }
-
-  guest_accelerator {
-    type = "nvidia-tesla-t4" # <<<<<<<<<< Try changing this to nvidia-tesla-p4 to compare the costs
-    count = 4
-  }
-
-  labels = {
-    environment = "production"
-    service = "web-app"
-  }
+# Database
+database_vm_instance_config = {
+  name             = "kasm-database"
+  machine_type     = "e2-standard-2"
+  disk_size_gb     = 50
+  description      = "Kasm database with Terraform"
+  disk_auto_delete = true
+  disk_type        = "pd-balanced"
+  instance_role    = "database"
 }
 
-resource "google_cloudfunctions_function" "my_function" {
-  runtime = "nodejs20"
-  name = "test"
-  available_memory_mb = 512
+# Agent
+number_of_agents_per_region = 3
+enable_agent_nat_gateway    = false
+agent_vm_instance_config = {
+  name_prefix      = "kasm-static-agent"
+  machine_type     = "e2-standard-2"
+  disk_size_gb     = 100
+  description      = "Kasm Static agent with Terraform"
+  disk_auto_delete = true
+  disk_type        = "pd-balanced"
+  instance_role    = "agent"
+}
 
-  labels = {
-    environment = "Prod"
-  }
+# Connection Proxy (Guac)
+deploy_connection_proxy = false
+deploy_windows_hosts    = false ## NOTE: This only creates the Windows subnet and firewall rules, it does NOT deploy Windows VMs
+cpx_vm_instance_config = {
+  machine_type = "e2-standard-2"
+  disk_size_gb = "50"
+  disk_type    = "pd-standard"
 }
